581848e1df5f647a964cc3aa0435424dc6b9ef17
bhek <bhegarty37@gmail.com>
2015-03-07 13:26:47 +0000

    back projection sample code works - needs to be tidied up

--- OpenCV/OpenCV/main.cpp
+++ OpenCV/OpenCV/main.cpp
@@ -10,7 +10,12 @@ void showImage(char* name, Mat image);
 void findSign(Mat image);
 void findSign(Mat image, Mat temp);
 void backProject(Mat image, Mat yellow);
+void backProjection(Mat image, Mat yellow);
+void Hist_and_Backproj(int, void*);
 void digitRecognition(Mat image);
+
+Mat src; Mat hsv; Mat hue; Mat hue2;
+int bins = 180;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -43,7 +48,10 @@ int _tmain(int argc, _TCHAR* argv[])
 	Mat sign = loadImage(testLocation, testFiles[6]);
 	Mat yellow = loadImage(templateLocation, templateFiles[4]);
 
-	backProject(sign, yellow);
+	//backProject(sign, yellow);
+
+	//backProjection(sign, yellow);
+	backProjection(yellow, sign);
 
 	/*int numberOfTestImages = sizeof(testFiles) / sizeof(testFiles[0]);
 	Mat* busStops = loadImages(numberOfTestImages, testLocation, testFiles);
@@ -133,13 +141,75 @@ void backProject(Mat image, Mat yellow) {
 	calcBackProject(&hue, 1, 0, hist, backProj, &ranges, 1, true);
 
 	showImage("Back project", backProj);
-	waitKey(0);
-
-	showImage("Stop", hsv);
-	imshow("Yellow", hsvt);
+	showImage("Stop", hue);
+	//imshow("Yellow", hsvt);
 	waitKey(0);
 
 	return;
+}
+
+void backProjection(Mat image, Mat yellow) {
+
+
+	/** @function main */
+	/// Read the image
+	//src = imread(argv[1], 1);
+	image.copyTo(src);
+	/// Transform it to HSV
+	cvtColor(image, hsv, CV_BGR2HSV);
+
+	/// Use only the Hue value
+	hue.create(hsv.size(), hsv.depth());
+	int ch[] = { 0, 0 };
+	mixChannels(&hsv, 1, &hue, 1, ch, 1);
+
+	Mat hsv2;
+	cvtColor(yellow, hsv2, CV_BGR2HSV);
+
+	hue2.create(hsv2.size(), hsv2.depth());
+	int ch2[] = { 0, 0 };
+	mixChannels(&hsv2, 1, & hue2, 1, ch2, 1);
+
+	/// Create Trackbar to enter the number of bins
+	char* window_image = "Source image";
+	namedWindow(window_image, CV_WINDOW_AUTOSIZE);
+	createTrackbar("* Hue  bins: ", window_image, &bins, 180, Hist_and_Backproj);
+	Hist_and_Backproj(0, 0);
+
+	/// Show the image
+	imshow(window_image, src);
+
+	/// Wait until user exits the program
+	waitKey(0);
+	return;
+}
+
+void Hist_and_Backproj(int, void*) {
+	MatND hist;
+	int histSize = MAX( bins, 2 );
+	float hue_range[] = { 0, 180 };
+	const float* ranges = { hue_range };
+
+	/// Get the Histogram and normalize it
+	calcHist( &hue, 1, 0, Mat(), hist, 1, &histSize, &ranges, true, false );
+	normalize( hist, hist, 0, 255, NORM_MINMAX, -1, Mat() );
+
+	/// Get Backprojection
+	MatND backproj;
+	calcBackProject( &hue2, 1, 0, hist, backproj, &ranges, 1, true );
+
+	/// Draw the backproj
+	imshow( "BackProj", backproj );
+
+	/// Draw the histogram
+	int w = 400; int h = 400;
+	int bin_w = cvRound( (double) w / histSize );
+	Mat histImg = Mat::zeros( w, h, CV_8UC3 );
+
+	for( int i = 0; i < bins; i ++ )
+		{ rectangle( histImg, Point( i*bin_w, h ), Point( (i+1)*bin_w, h - cvRound( hist.at<float>(i)*h/255.0 ) ), Scalar( 0, 0, 255 ), -1 ); }
+
+	showImage( "Histogram", histImg );
 }
 
 void digitRecognition(Mat image) {
