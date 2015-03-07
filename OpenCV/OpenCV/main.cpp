931733aba66abceea27ecb1c43acd4efe9e44546
bhek <bhegarty37@gmail.com>
2015-03-07 14:54:48 +0000

    tidied up 'backProjection()', removed sample code & outputs both 'findSign()' and 'backProjection()' outputs

--- OpenCV/OpenCV/main.cpp
+++ OpenCV/OpenCV/main.cpp
@@ -7,15 +7,15 @@
 Mat loadImage(char* location, char* file);
 Mat* loadImages(int numberOfImages, char* location, char** files);
 void showImage(char* name, Mat image);
-void findSign(Mat image);
-void findSign(Mat image, Mat temp);
-void backProject(Mat image, Mat yellow);
-void backProjection(Mat image, Mat yellow);
-void Hist_and_Backproj(int, void*);
+Mat findSign(Mat image);
+MatND backProjection(Mat image, Mat yellow);
+Mat getHue(Mat image);
+/*void backProjection(Mat image, Mat yellow);
+void Hist_and_Backproj(int, void*);*/
 void digitRecognition(Mat image);
 
-Mat src; Mat hsv; Mat hue; Mat hue2;
-int bins = 180;
+/*Mat src; Mat hsv; Mat hue; Mat hue2;
+int bins = 180;*/
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -48,11 +48,13 @@ int _tmain(int argc, _TCHAR* argv[])
 	Mat sign = loadImage(testLocation, testFiles[6]);
 	Mat yellow = loadImage(templateLocation, templateFiles[4]);
 
-	//backProject(sign, yellow);
-
-	//backProjection(sign, yellow);
 	showImage("Sign", sign);
-	backProjection(yellow, sign);
+	Mat hsvSign = findSign(sign);
+	MatND backProjSign = backProjection(sign, yellow);
+
+	showImage("HSV", hsvSign);
+	showImage("Back Projection", backProjSign);
+	waitKey(0);
 
 	/*int numberOfTestImages = sizeof(testFiles) / sizeof(testFiles[0]);
 	Mat* busStops = loadImages(numberOfTestImages, testLocation, testFiles);
@@ -108,85 +110,45 @@ void showImage(char* name, Mat image) {
 	return;
 }
 
-void findSign(Mat image) {
+Mat findSign(Mat image) {
 	Mat hsv;
 	cvtColor(image, hsv, CV_BGR2HSV);
 	inRange(hsv, Scalar(15, 135, 140), Scalar(30, 255, 255), hsv);
-	showImage("Result", hsv);
 
-	return;
+	return hsv;
 }
 
-void backProject(Mat image, Mat yellow) {
-	
+MatND backProjection(Mat image, Mat yellow) {
+	Mat imageHue = getHue(image);
+	Mat yellowHue = getHue(yellow);
 
-	return;
+	MatND hist;
+	int histSize = 180;
+	float hue_range[] = { 0, 180 };
+	const float* ranges = { hue_range };
+
+	calcHist(&yellowHue, 1, 0, Mat(), hist, 1, &histSize, &ranges, true, false);
+	normalize(hist, hist, 0, 255, NORM_MINMAX, -1, Mat());
+
+	MatND backProject;
+	calcBackProject(&imageHue, 1, 0, hist, backProject, &ranges, 1, true);
+
+	return backProject;
 }
 
-void backProjection(Mat image, Mat yellow) {
-	/** @function main */
-	/// Read the image
-	//src = imread(argv[1], 1);
-	image.copyTo(src);
-	/// Transform it to HSV
+Mat getHue(Mat image) {
+	Mat hsv, hue;
 	cvtColor(image, hsv, CV_BGR2HSV);
 
-	/// Use only the Hue value
 	hue.create(hsv.size(), hsv.depth());
 	int ch[] = { 0, 0 };
 	mixChannels(&hsv, 1, &hue, 1, ch, 1);
 
-	Mat hsv2;
-	cvtColor(yellow, hsv2, CV_BGR2HSV);
-
-	hue2.create(hsv2.size(), hsv2.depth());
-	int ch2[] = { 0, 0 };
-	mixChannels(&hsv2, 1, & hue2, 1, ch2, 1);
-
-	/// Create Trackbar to enter the number of bins
-	char* window_image = "Source image";
-	namedWindow(window_image, CV_WINDOW_AUTOSIZE);
-	createTrackbar("* Hue  bins: ", window_image, &bins, 180, Hist_and_Backproj);
-	Hist_and_Backproj(0, 0);
-
-	/// Show the image
-	imshow(window_image, src);
-
-	/// Wait until user exits the program
-	waitKey(0);
-	return;
-}
-
-void Hist_and_Backproj(int, void*) {
-	MatND hist;
-	int histSize = MAX( bins, 2 );
-	float hue_range[] = { 0, 180 };
-	const float* ranges = { hue_range };
-
-	/// Get the Histogram and normalize it
-	calcHist( &hue, 1, 0, Mat(), hist, 1, &histSize, &ranges, true, false );
-	normalize( hist, hist, 0, 255, NORM_MINMAX, -1, Mat() );
-
-	/// Get Backprojection
-	MatND backproj;
-	calcBackProject( &hue2, 1, 0, hist, backproj, &ranges, 1, true );
-
-	/// Draw the backproj
-	showImage( "BackProj", backproj );
-
-	/// Draw the histogram
-	int w = 400; int h = 400;
-	int bin_w = cvRound( (double) w / histSize );
-	Mat histImg = Mat::zeros( w, h, CV_8UC3 );
-
-	for( int i = 0; i < bins; i ++ )
-		{ rectangle( histImg, Point( i*bin_w, h ), Point( (i+1)*bin_w, h - cvRound( hist.at<float>(i)*h/255.0 ) ), Scalar( 0, 0, 255 ), -1 ); }
-
-	showImage( "Histogram", histImg );
+	return hue;
 }
 
 void digitRecognition(Mat image) {
-	// TODO: complete function
+	// TODO: recognise numbers from sign
 
 	return;
 }
