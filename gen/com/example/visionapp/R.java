7f9e4a78ca5433c36649182c3bac12b3a0d8174c
bhek <bhegarty37@gmail.com>
2015-03-10 18:47:40 +0000

    added blank 'ImageActivity' for image processing & displaying

--- gen/com/example/visionapp/R.java
+++ gen/com/example/visionapp/R.java
@@ -66,14 +66,17 @@ containing a value of this type.
         public static final int takepicture=0x7f040007;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_results=0x7f030001;
-        public static final int fragment_main=0x7f030002;
-        public static final int fragment_results=0x7f030003;
+        public static final int activity_image=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_results=0x7f030002;
+        public static final int fragment_image=0x7f030003;
+        public static final int fragment_main=0x7f030004;
+        public static final int fragment_results=0x7f030005;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
-        public static final int results=0x7f080001;
+        public static final int image=0x7f080000;
+        public static final int main=0x7f080001;
+        public static final int results=0x7f080002;
     }
     public static final class string {
         public static final int action_settings=0x7f060002;
@@ -85,6 +88,7 @@ containing a value of this type.
         public static final int search=0x7f060004;
         public static final int stop=0x7f060003;
         public static final int take_picture=0x7f060007;
+        public static final int title_activity_image=0x7f06000a;
         public static final int title_activity_results=0x7f060006;
     }
     public static final class style {
